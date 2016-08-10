3ba604e57d9fa11f52ac970a7fb2f3ee4e979bc9
chandrakanth <n.chandrakanth0@gmail.com>
2016-08-10 01:45:53 +0000

    edit and delete action

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+    
     def index
         @posts = Post.all.order('created_at DESC');
     end
@@ -20,9 +21,31 @@ class PostsController < ApplicationController
         @post = Post.find(params[:id])
     end
     
+    def edit
+         @post = Post.find(params[:id])
+    end
+    
+    def update
+        @post = Post.find(params[:id])
+       if @post.update(post_params)
+           redirect_to @post
+       else
+           render 'edit'
+       end
+    end
+    
+    def destroy
+        @post = Post.find(params[:id])
+        @post.destroy
+        redirect_to root_path
+    end
+    
     private
     
     def post_params
         params.require(:post).permit(:title,:body)
+    end
+    def find_post
+        @post = Post.find(params[:id])
     end
 end
